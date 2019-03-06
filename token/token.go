62b96778b9e7c286d849058b8ad1334291f39312
Ashok Gelal <ashok@metageek.net>
2019-03-05 23:25:42 -0700

    Implement lexer. WIP

---
+++ token/token.go
@@ -0,0 +1,63 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type TokenType
+	Literal string
+}
+
+var keywords = map[string]TokenType {
+	"fn": FUNCTION,
+	"let": LET,
+	"true": TRUE,
+	"false": FALSE,
+	"if": IF,
+	"else": ELSE,
+	"return": RETURN,
+}
+
+const (
+	ILLEGAL = "ILLEGAl"
+	EOF = "EOF"
+	// identifiers + literals
+	IDENT = "IDENT" 	// add, foobar, x, y
+	INT = "INT" 		// 1235
+
+	// Operators
+	ASSIGN = "="
+	PLUS = "+"
+	MINUS = "-"
+	BANG = "/"
+	ASTERISK = "*"
+	SLASH = "/"
+	EQ = "=="
+	NOT_EQ = "!="
+
+	LT = "<"
+	GT = ">"
+
+	// Delimeters
+	COMMA = ","
+	SEMICOLON = ";"
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET = "LET"
+	TRUE = "TRUE"
+	FALSE = "FALSE"
+	IF = "IF"
+	ELSE = "ELSE"
+	RETURN = "RETURN"
+)
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
